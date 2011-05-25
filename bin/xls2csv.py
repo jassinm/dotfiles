4841677fab9040e68274c9d5bcf6680c369457ce
locojay <locojay@dev.dev>
2011-05-25 12:59:34 -0400

    xls2csv.py

---
+++ bin/xls2csv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+File: xls2csv.py
+Created: Wed 25 May 2011 08:56:06 AM EDT
+Last Modified: Wed 25 May 2011 08:56:06 AM EDT
+Author: Jassin MEKNASSI
+Description: converts an xls file to a csv file
+'''
+
+from xlrd import open_workbook
+import sys
+import csv
+
+outfile = open(sys.argv[2],'w')
+csvwriter = csv.writer(outfile)
+
+wbk = open_workbook(sys.argv[1],encoding_override='utf-8')
+sheet = wbk.sheet_by_index(0)
+
+def encodeh(val):
+    if isinstance(val,unicode):
+        val = val.encode('utf-8')
+        val = val.replace('\n','')
+        val = val.replace(',','')
+    return val
+
+for rowidx in range(0,sheet.nrows):
+    rowvalues = sheet.row_values(rowidx)
+    rowvalues = [encodeh(r) for r in rowvalues]
+    csvwriter.writerow(rowvalues)
+
+outfile.close()
