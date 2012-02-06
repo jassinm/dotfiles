24601dceb593998b330ea12649a3ea46e692fc8b
locojay <locojaydev@dev.com>
2012-02-06 12:50:56 -0500

    cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,72 @@
-f=$(pwd)
+DOTFILES=$(pwd)
+SYMLINK="ln -fs"
+
 git submodule init
 git submodule update
-read -p "install vim' (y/n)?"
-if [ "$REPLY" == "y" ]
-then
-	cd $HOME/.dotfiles/vim
-	sh bootstrap.sh
-fi
-read -p "install oh-my-zsh' (y/n)?"
-if [ "$REPLY" == "y" ]
-then
-	cd $HOME/.dotfiles/oh-my-zsh/
-	sh bootstrap.sh
-fi
-cd $HOME/.dotfiles/
-sudo gem install teamocil
-rake install
-mkdir -p $HOME/.ipython
+
+function link_file {
+    source="${DOTFILEs}/$1"
+    target="${HOME}/${1/_/.}"
+
+    if [ -e "${target}" ]; then
+        mv $target $target.bak
+    fi
+
+    ln -sf ${source} ${target}
+}
+
+
+link_file irssi
+#read -p "install vim' (y/n)?"
+#if [ "$REPLY" == "y" ]
+#then
+    #cd $DOTFILES/vim
+    #sh bootstrap.sh
+    #rm $HOME/.vim
+    #$SYMLINK $DOTFILES/vim $HOME/.vim
+#fi
+#read -p "install oh-my-zsh' (y/n)?"
+#if [ "$REPLY" == "y" ]
+#then
+    #cd $DOTFILES/oh-my-zsh/
+    #sh bootstrap.sh
+#fi
+
+cd $DOTFILES
+
+
+##inputrc
+#$SYMLINK $DOTFILES/inputrc $HOME/.inputrc
+
+##tmux
+#$SYMLINK $DOTFILES/tmux.conf $HOME/.tmux.conf
+##teamocil
+#$SYMLINK $DOTFILES/teamocil $HOME/.teamocil
+
+##irssi
+#$SYMLINK $DOTFILES/irssi $HOME/.irssi
+
+##ipython
+#$SYMLINK $DOTFILES/ipython $HOME/.ipython
+
+
+##git
+#$SYMLINK $DOTFILES/gitignore $HOME/.gitignore
+#$SYNLINK $DOTFILES/gitconfig $HOME/.gitconfig
+
+##ack
+#$SYMLINK $DOTFILES/ackrc $HOME/.ackrc
+
+##bash
+#$SYMLINK $DOTFILES/bash $HOME/.bash
+#$SYMLINK $DOTFILES/bash_profile $HOME/.bash_profile
+#$SYMLINK $DOTFILES/bashrc $HOME/.bashrc
+
+##bin
+#$SYMLINK $DOTFILES/bin $HOME/.bin
+
+##zlogin
+#$SYMLINK $DOTFILES/zlogin $HOME/.zlogin
+
+
+##sudo gem install teamocil
