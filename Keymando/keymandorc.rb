d6e479ad5dedc0bc84cc4f58b9d24c28f139e96b
locojay <locojaydev@dev.com>
2012-02-27 21:21:10 -0500

    updating keymando

--- Keymando/keymandorc.rb
+++ Keymando/keymandorc.rb
@@ -48,20 +48,46 @@ end
 only /Preview/ do
     map "<Ctrl-g>", lambda{send("<Alt-Cmd-g>");}
     map "/" , "<Cmd-f>"
-    #map "<Ctrl-d>", "<PageDown>"
-    #map "<Ctrl-u>", "<PageUp>"
-    #map "h", "<Left>"
-    #map "j", "<Down>"
-    #map "k", "<Up>"
-    #map "l", "<Right>"
-    #map "gg" , lambda { send("<Home>");}
-    #map "G" , lambda { send("<End>");}
+    map "<Ctrl-d>", "<PageDown>"
+    map "<Ctrl-u>", "<PageUp>"
+    map "h", "<Left>"
+    map "j", "<Down>"
+    map "k", "<Up>"
+    map "l", "<Right>"
+    map "gg", "<Home>"
+    map "G","<End>"
     #map "gg", "<Fn-Left>"
     #map "G", "<Fn-Right>"
 end
 
 
-# map "<Cmd-l>", trigger-app
+command "Reload" {alert('Reload Successfully') if reload}
+#
+command "Mailbox locojaydev" do
+     activate('Mail')
+end
+
+
+Commands.register LeftClick.instance,
+  RightClick.instance,
+  RunHistoryItem.instance,
+  LockTheScreen.instance,
+  UiControls.instance
+  RunLastCommand.instance
+
+
+
+map "<Cmd-.>",RunLastCommand.instance
+# map "<Cmd-d>", current_app_windows
+
+# map "<Cmd-p>", hit_a_hint
+
+map "<Cmd-k>", run_registered_command
+# map "<Cmd- >", launch_app
+# map "<Cmd-d>", current_app_windows
+# map "<Cmd-f>", trigger_app
+
+
 
 # -----------------------------------------------------------
 # Visit http://keymando.com to see what Keymando can do!
