a367a8ddae304aebd7ec9a356d07386db0ae7fd6
locojaydev <locojaydev@dev.com>
2012-03-08 12:47:15 -0500

    updating using keychain (sjl)

--- bin/offlineimap.py
+++ bin/offlineimap.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 import re
-import os
+import subprocess
 
 
-def get_authinfo_password(account):
-    p = re.compile('%s:\s+(\w+)' % account)
-    authinfo = os.popen("gpg -q -d ~/.secret/.passwd.gpg").read()
-    return p.search(authinfo).group(1)
+def get_keychain_pass(account=None, server=None):
+    params = {
+        'security': '/usr/bin/security',
+        'command': 'find-internet-password',
+        'account': account,
+        'server': server,
+        'keychain': '~/Library/Keychains/login.keychain',
+    }
+    command = "sudo -u locojay %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    outtext = [l for l in output.splitlines()
+               if l.startswith('password: ')][0]
+
+    return re.match(r'password: "(.*)"', outtext).group(1)
