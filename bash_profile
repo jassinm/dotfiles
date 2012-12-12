b69cfe992629505ddc1aafd86d333aa0afd2c229
locojaydev <locojaydev@dev.com>
2012-12-11 19:03:38 -0500

    cleanup dash

--- bash_profile
+++
@@ -1,7 +0,0 @@
-if [ -f ~/.bashrc ]; then
-  source ~/.bashrc
-fi
-
-export NARWHAL_ENGINE=jsc
-
-[[ -s "/Users/locojay/.rvm/scripts/rvm" ]] && source "/Users/locojay/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,19 @@
-#!/usr/bin/env bash
-DOTFILES=$(pwd)
+#!/usr/bin/env sh
 
+DOTFILES="$(pwd)"
 
-function link_file {
-    source="${DOTFILES}/$1"
-    target="${HOME}/.${1}"
-
-    ln -nfs ${source} ${target}
+link_file () {
+    source_="${DOTFILES}/$1"
+    target_="${HOME}/.${1}"
+    ln -nfs ${source_} ${target_}
 }
 
-EXCLUDE="README.rst\|Keymando\|aliases.zsh\|bootstrap.sh\|vim\|lein2\|zshrc"
+excludes="README.rst\|Keymando\|aliases.zsh\|bootstrap.sh\|vim\|lein2\|zshrc"
 
-for i in `ls | grep -v $EXCLUDE`
+for file_or_folder in `find . -maxdepth 1 -name '[!.]*' | sed "s|^\./||" | grep -v "$excludes"`
 do
-    link_file $i
+    link_file "$file_or_folder"
 done
 
-ln -nfs $DOTFILES/lein2/project.clj $HOME/.lein/project.clj
-
-read -p "install vim' (y/n)?"
-if [ "$REPLY" == "y" ]
-then
-    link_file vim
-    ln -nfs $DOTFILES/vim/vimrc $HOME/.vimrc
-fi
+mkdir -p "$HOME"/.lein
+ln -nfs "$DOTFILES"/lein2/project.clj "$HOME"/.lein/project.clj
