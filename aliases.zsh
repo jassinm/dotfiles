5df7079186e94139f326ff3fa2a1d0d20beb533b
locojay <locojaydev@gmail.com>
2012-12-14 17:16:14 -0500

    some zsh stuff

--- aliases.zsh
+++ aliases.zsh
@@ -1,13 +1,21 @@
+#git
 alias gst='git status'
 alias gba='git branch -a'
 alias gcaa="git commit -a --amend -C HEAD"
+alias gpl='git pull'
+#tmux
 alias tls='tmux list-sessions'
 alias ta='tmux attach -t'
+
 alias clj='rlwrap -a dummy clj'
 alias mongo='rlwrap -a dummy mongo'
+
 alias zs='source ~/.zshrc'
 alias pyclean='find . -type f -name "*.py[co]" -delete'
 alias mkproject='mkproject -t locodev'
 alias cdp='cd $PROJECT_HOME'
 alias top='htop'
 alias ipynb="ipython notebook --ip='*' --pylab=inline"
+
+alias ka9='killall -9'
+alias k9='kill -9'
