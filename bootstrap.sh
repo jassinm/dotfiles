1a81f8f34e96e694c24af30350299fa42d736b59
locojay <locojaydev@gmail.com>
2012-12-14 19:56:05 -0500

    some more cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,12 +8,23 @@ link_file () {
     ln -nfs ${source_} ${target_}
 }
 
-excludes="README.rst\|Keymando\|aliases.zsh\|bootstrap.sh\|vim\|lein2\|zshrc"
+excludes="README.rst\|Keymando\|aliases.zsh\|bootstrap.sh\|vim\|lein2|\prezto"
 
-for file_or_folder in `find . -maxdepth 1 -name '[!.]*' | sed "s|^\./||" | grep -v "$excludes"`
+for file_or_folder in `find . -maxdepth 1 -name '[!.]*' | sed "s|^\./||" | grep -v "$excludes"`;
 do
     link_file "$file_or_folder"
 done
 
 mkdir -p "$HOME"/.lein
 ln -nfs "$DOTFILES"/lein2/project.clj "$HOME"/.lein/project.clj
+#vim
+mkdir -p "$DOTFILES"/vim/tmp/undo
+mkdir -p "$DOTFILES"/vim/tmp/backup
+mkdir -p "$DOTFILES"/vim/tmp/swap
+ln -nfs "$DOTFILES"/vim $HOME/.vim
+#
+ln -nfs "$DOTFILES"/prezto $HOME/.zprezto
+
+# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+#       ln -nfs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+# done
