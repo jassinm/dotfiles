50064e40c677fecd1f095db39ec0bb5329b6c4ec
locojay <locojaydev@gmail.com>
2021-05-23 15:01:05 +0200

    cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -158,7 +158,7 @@ link_file "$DOTFILES/doom.d" "$HOME/.doom.d"
 #
 
 #link_file "$DOTFILES/ipython" "$HOME/.ipython"
-mkdir -p "$HTOME/.ipython/profile_default"
+mkdir -p "$HOME/.ipython/profile_default"
 link_file "$DOTFILES/ipython/ipython/profile_default/ipython_config.py" "$HOME/.ipython/profile_default/ipython_config.py"
 #
 ##emacs
