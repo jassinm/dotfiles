c50ca37c74050d7b3d2b883faaef0c19a084e2fc
locojay <locojaydev@gmail.com>
2021-12-25 21:49:50 +0100

    some more

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,15 @@ require('completion')
 require('comment')
 require('tree')
 -- --require('dashboard')
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.org = {
+  install_info = {
+    url = 'https://github.com/milisims/tree-sitter-org',
+    revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
+    files = {'src/parser.c', 'src/scanner.cc'},
+  },
+  filetype = 'org',
+}
 --
 require'nvim-treesitter.configs'.setup {
   rainbow = {
@@ -13,9 +22,11 @@ require'nvim-treesitter.configs'.setup {
     extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
   },
   highlight = {
-    enable = true
-
-  }
+    enable = true,
+    disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
+  },
+  ensure_installed = {'org'}, -- Or run :TSUpdate org
 }
 require('nvim-autopairs').setup()
 --
