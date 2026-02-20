8868f546f9b0ee14e709130fe2a56cfab0d30d27
locojay <locojaydev@gmail.com>
2026-02-20 20:55:53 +0100

    more

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1762617240/share/lua/5.1/?.lua;/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1762617240/share/lua/5.1/?/init.lua;/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1762617240/lib/luarocks/rocks-5.1/?.lua;/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1762617240/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1762617240/lib/lua/5.1/?.so"
+local package_path_str = "/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1770848219/share/lua/5.1/?.lua;/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1770848219/share/lua/5.1/?/init.lua;/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1770848219/lib/luarocks/rocks-5.1/?.lua;/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1770848219/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/jassinm/.cache/nvim/packer_hererocks/2.1.1770848219/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
