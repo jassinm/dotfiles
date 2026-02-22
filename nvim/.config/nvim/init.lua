16d1f6f64300cec26217355899458dbe48861d4e
locojay <locojaydev@gmail.com>
2026-02-22 09:40:27 +0100

    cleanup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,63 +1,44 @@
-require('plugins')
+require("plugins")
 
-require('code')
-require('completion')
-require('lv-telescope')
-require('tree')
+require("code")
+require("completion")
+require("lv-telescope")
+require("tree")
 
-
-require('nvim-autopairs').setup()
---
 --require('gitsigns').setup()
 --
 vim.o.termguicolors = true
 --require('colorizer').setup()
 
-require('lualine').setup {
-  options = {
-    -- ... your lualine config
-    theme = 'gruvbox'
-    -- ... your lualine config
-  }
-}
-
--- require('project_nvim').setup {
---
--- }
-
-require'bufferline'.setup({
-  --icons = false
-})
 
 -- init.lua
 vim.g.symbols_outline = {
-    highlight_hovered_item = true,
-    show_guides = true,
-    position = 'right',
-    keymaps = {
-        close = "<Esc>",
-        goto_location = "<Cr>",
-        focus_location = "o",
-        hover_symbol = "<C-space>",
-        rename_symbol = "r",
-        code_actions = "a",
-    },
-    lsp_blacklist = {},
+	highlight_hovered_item = true,
+	show_guides = true,
+	position = "right",
+	keymaps = {
+		close = "<Esc>",
+		goto_location = "<Cr>",
+		focus_location = "o",
+		hover_symbol = "<C-space>",
+		rename_symbol = "r",
+		code_actions = "a",
+	},
+	lsp_blacklist = {},
 }
 -- Load the ColorScheme
 vim.opt.background = "dark"
 require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  --italic = true, -- will make italic comments and special strings
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  contrast = "hard", -- can be "hard" or "light"
-  overrides = {
-    },
+	undercurl = true,
+	underline = true,
+	bold = true,
+	--italic = true, -- will make italic comments and special strings
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	invert_intend_guides = false,
+	contrast = "hard", -- can be "hard" or "light"
+	overrides = {},
 })
 vim.cmd([[colorscheme gruvbox]])
 --
@@ -66,11 +47,11 @@ vim.o.guifont = "PragmataPro Nerd Font:h19"
 --
 -- -- Settings
 -- --
-vim.g.mapleader = ','
-vim.g.maplocalleader = '\\'
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
 vim.cmd([[map ; :]])
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', 'kj', '<ESC>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("i", "kj", "<ESC>", { noremap = true, silent = true })
 --
 vim.o.fileencoding = "utf-8" -- The encoding written to file
 -- Make line numbers default
@@ -82,11 +63,10 @@ vim.opt.relativenumber = true
 vim.o.mouse = "a"
 --
 -- --Save undo history
-vim.cmd[[ set undofile ]]
-
+vim.cmd([[ set undofile ]])
 
 --
-vim.cmd[[autocmd BufWritePre * :%s/\s\+$//e]] -- remove trailing whitespaces
+vim.cmd([[autocmd BufWritePre * :%s/\s\+$//e]]) -- remove trailing whitespaces
 
 --
 -- Default Tabs, spaces, wrapping
@@ -96,7 +76,7 @@ vim.o.softtabstop = 4
 vim.o.expandtab = true
 vim.o.wrap = true
 -- --vim.o.textwidth = 85
-vim.o.formatoptions = 'qrn1'
+vim.o.formatoptions = "qrn1"
 
 vim.opt.scrolloff = 8
 --
@@ -120,65 +100,126 @@ vim.o.updatetime = 300 -- Faster completion
 vim.o.clipboard = "unnamedplus" -- Copy paste between vim and everything else
 
 -- Highlight on yank
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+	[[
   augroup YankHighlight
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
   augroup end
-]], false)
+]],
+	false
+)
 --
 ----
-vim.api.nvim_set_keymap('n', '<leader>ev', ':vsplit $HOME/.config/nvim/init.lua<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ev", ":vsplit $HOME/.config/nvim/init.lua<CR>", { noremap = true, silent = true })
 --
 --Buffers
 -- -- vim.api.nvim_set_keymap('n', '<leader>bn', ':BufferNext<CR>', { noremap = true, silent = true })
 -- -- vim.api.nvim_set_keymap('n', '<leader>bp', ':BufferPrevious<CR>', { noremap = true, silent = true })
 -- -- vim.api.nvim_set_keymap('n', '<leader>bc', ':BufferClose<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<TAB>', ':BufferNext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferPrevious<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-x>', ':BufferClose<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<TAB>", ":BufferNext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-TAB>", ":BufferPrevious<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-x>", ":BufferClose<CR>", { noremap = true, silent = true })
 -- --
 -- --vim.o.splitright = true -- Vertical splits will automatically be to the right
 -- --vim.o.splitbelow = true -- Horizontal splits will automatically be below
 --
 -- Window commands
-vim.api.nvim_set_keymap('n', '<leader>wh', ':wincmd h<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>wj', ':wincmd j<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>wk', ':wincmd k<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>wl', ':wincmd l<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>ww', ':wincmd w<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>wr', ':wincmd r<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>wo', ':wincmd o<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>wx', ':wincmd x<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>wq', ':wincmd q<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>ws', ':split <CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>wv', ':vsplit <CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>wh", ":wincmd h<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wj", ":wincmd j<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wk", ":wincmd k<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wl", ":wincmd l<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ww", ":wincmd w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wr", ":wincmd r<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wo", ":wincmd o<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wx", ":wincmd x<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wq", ":wincmd q<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ws", ":split <CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wv", ":vsplit <CR>", { noremap = true, silent = true })
 
 -- Quickfix
 --
 -- Better nav for omnicomplete
-vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
-vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
+vim.cmd('inoremap <expr> <c-j> ("\\<C-n>")')
+vim.cmd('inoremap <expr> <c-k> ("\\<C-p>")')
 --
 -- -- Explore
-vim.api.nvim_set_keymap('n', '<leader>ft', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fr', ':RnvimrToggle<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>ft", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fr", ":RnvimrToggle<CR>", { noremap = true, silent = true })
 --
-vim.api.nvim_set_keymap('n', '<leader>ff', ":lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fg', ":lua require('telescope.builtin').live_grep()<cr>", { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>sp', ":lua require('telescope.builtin').live_grep()<cr>", { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>bb', ":lua require('telescope.builtin').buffers()<cr>", { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fb', ":lua require('telescope.builtin').buffers()<cr>", { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fh', ":lua require('telescope.builtin').help_tags()<cr>", { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gc', [[<cmd>lua require('telescope.builtin').git_commits()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gb', [[<cmd>lua require('telescope.builtin').git_branches()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gs', [[<cmd>lua require('telescope.builtin').git_status()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>gp', [[<cmd>lua require('telescope.builtin').git_bcommits()<cr>]], { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>ss', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>ff",
+	":lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>fg",
+	":lua require('telescope.builtin').live_grep()<cr>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>sp",
+	":lua require('telescope.builtin').live_grep()<cr>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>bb",
+	":lua require('telescope.builtin').buffers()<cr>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>fb",
+	":lua require('telescope.builtin').buffers()<cr>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>fh",
+	":lua require('telescope.builtin').help_tags()<cr>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>gc",
+	[[<cmd>lua require('telescope.builtin').git_commits()<cr>]],
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>gb",
+	[[<cmd>lua require('telescope.builtin').git_branches()<cr>]],
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>gs",
+	[[<cmd>lua require('telescope.builtin').git_status()<cr>]],
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>gp",
+	[[<cmd>lua require('telescope.builtin').git_bcommits()<cr>]],
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>ss",
+	[[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>]],
+	{ noremap = true, silent = true }
+)
 
-vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<cr>]], { noremap = true, silent = true})
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>sd",
+	[[<cmd>lua require('telescope.builtin').grep_string()<cr>]],
+	{ noremap = true, silent = true }
+)
 --
 --
-vim.api.nvim_set_keymap('n', '<leader>K', ':SymbolsOutline<CR>', {noremap = true, silent = true})
-
-
+vim.api.nvim_set_keymap("n", "<leader>K", ":SymbolsOutline<CR>", { noremap = true, silent = true })
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,83 +23,120 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {},
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
-    },
-   -- add your plugins here
-   -- Languages support
-   -- "sheerun/vim-polyglot"
-    -- Color
-    "ellisonleao/gruvbox.nvim",
-    -- Dashboard
-    "goolord/alpha-nvim",
-    -- "mhinz/vim-startify"
-    -- Statusline
-    {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
-    --Tpope
-    -- 'tpope/vim-commentary'
-    "tpope/vim-surround",
-    "tpope/vim-fugitive",
-    "tpope/vim-unimpaired",
-    {
-      'nvim-treesitter/nvim-treesitter',
-      lazy = false,
-      build = ':TSUpdate'
-    },
-    'windwp/nvim-autopairs',
-    -- Eplorer
-   "kyazdani42/nvim-tree.lua",
-    -- Language Server
-    "neovim/nvim-lspconfig",
-    -- Telescope - find stuff
-    {
-        "nvim-telescope/telescope.nvim", version = "*",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            -- optional but recommended
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        }
-    },
-    -- Formating
-    "sbdchd/neoformat",
-    -- Comment
-    "terrortylor/nvim-comment",
+	spec = {
+		{
+			"mason-org/mason-lspconfig.nvim",
+			opts = {},
+			dependencies = {
+				{ "mason-org/mason.nvim", opts = {} },
+				"neovim/nvim-lspconfig",
+			},
+		},
+		-- add your plugins here
+		-- Languages support
+		-- "sheerun/vim-polyglot"
+		-- Color
+		"ellisonleao/gruvbox.nvim",
+		-- Dashboard
+		{
+			"goolord/alpha-nvim",
+			-- dependencies = { 'nvim-mini/mini.icons' },
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				local startify = require("alpha.themes.startify")
+				-- available: devicons, mini, default is mini
+				-- if provider not loaded and enabled is true,
+				-- it will try to use another provider
+				startify.file_icons.provider = "devicons"
+				require("alpha").setup(startify.config)
+			end,
+		},
+		-- "mhinz/vim-startify"
+		-- Statusline
+		{
+			"nvim-lualine/lualine.nvim",
+            opts = {
+                theme = "gruvbox",
+            },
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+		},
+		{
+			"romgrk/barbar.nvim",
+			dependencies = {
+				"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+				"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+			},
+			init = function()
+				vim.g.barbar_auto_setup = false
+			end,
+			opts = {
+				-- lazy.nvim will automatically call setup for you.
+                -- put your options here, anything missing will use the default:
+				-- animation = true,
+				-- insert_at_start = true,
+				-- …etc.
+			},
+            version = '^1.0.0', -- optional: only update when a new 1.x version is released
+		},
 
-    -- Autocomplete
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    --
-    {
-    "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = "Trouble",
-    },
-    --
-    {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'}
+		--Tpope
+		-- 'tpope/vim-commentary'
+		"tpope/vim-surround",
+		"tpope/vim-fugitive",
+		"tpope/vim-unimpaired",
+		{
+			"nvim-treesitter/nvim-treesitter",
+			lazy = false,
+			build = ":TSUpdate",
+		},
+		{
+			"windwp/nvim-autopairs",
+			event = "InsertEnter",
+			config = true,
+			-- use opts = {} for passing setup options
+			-- this is equivalent to setup({}) function
+		},
+		-- Eplorer
+		"kyazdani42/nvim-tree.lua",
+		-- Language Server
+		"neovim/nvim-lspconfig",
+		-- Telescope - find stuff
+		{
+			"nvim-telescope/telescope.nvim",
+			version = "*",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				-- optional but recommended
+				{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			},
+		},
+		-- Formating
+		"sbdchd/neoformat",
+		-- Comment
+		"terrortylor/nvim-comment",
 
-
-
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+		-- Autocomplete
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		--
+		{
+			"folke/trouble.nvim",
+			opts = {}, -- for default options, refer to the configuration section for custom setup.
+			cmd = "Trouble",
+		},
+		--
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
 
---
+-- Old plugins check
 --         use "hrsh7th/vim-vsnip"
 --         use "rafamadriz/friendly-snippets"
 --
@@ -111,7 +148,6 @@ require("lazy").setup({
 --         use "simrat39/symbols-outline.nvim"
 --
 --         use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
---         use 'nvim-tree/nvim-web-devicons'
 --         use {
 --              'romgrk/barbar.nvim',
 --              requires = 'nvim-web-devicons'
@@ -140,7 +176,6 @@ require("lazy").setup({
 --         -- ---
 --         use {'lukas-reineke/indent-blankline.nvim', branch='master'}
 --         -- --
---         use 'ahmedkhalf/project.nvim'
 --         -- --
 --         use 'jpalardy/vim-slime'
 --
